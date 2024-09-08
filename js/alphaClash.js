c82119a651b7737fe40b61dde4f93f8c5845aed5
aref-360nobra <aref0171personal@gmail.com>
2024-09-08 15:50:05 +0600

    github making ready

--- js/alphaClash.js
+++ js/alphaClash.js
@@ -6,6 +6,16 @@ document.getElementById("playNowBtn").addEventListener("click", function () {
     document.getElementById("playGround").classList.remove("hidden");
     continueGame();
 });
+function handleKeyUpEvent(event) {
+    const playerPressed = event.key;
+    // console.log("player pressed", playerPressed);
+    // get expected pressed
+    const currentAlphabetElement = document.getElementById("currentAlphabet");
+    const targetLetter = currentAlphabetElement.innerText.toLowerCase();
+    // console.log(currentAlphabetElement.innerText);
+    console.log(playerPressed, targetLetter);
+}
+document.addEventListener("keyup", handleKeyUpEvent);
 
 function continueGame() {
     // generate a random alphabet
