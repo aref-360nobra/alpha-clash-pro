cabf1a6836987d74ecb4001282b505102073a24c
aref-360nobra <aref0171personal@gmail.com>
2024-09-08 16:45:33 +0600

    game almost done without scoreboard

--- js/alphaClash.js
+++ js/alphaClash.js
@@ -1,29 +1,43 @@
 document.getElementById("playNowBtn").addEventListener("click", function () {
-    // hide the home screen
-    const homeSection = document.getElementById("homeScreen");
-    homeSection.classList.add("hidden");
-    // show the playground
-    document.getElementById("playGround").classList.remove("hidden");
-    continueGame();
+  // hide the home screen
+  const homeSection = document.getElementById("homeScreen");
+  homeSection.classList.add("hidden");
+  // show the playground
+  document.getElementById("playGround").classList.remove("hidden");
+  continueGame();
 });
 function handleKeyUpEvent(event) {
-    const playerPressed = event.key;
-    // console.log("player pressed", playerPressed);
-    // get expected pressed
-    const currentAlphabetElement = document.getElementById("currentAlphabet");
-    const targetLetter = currentAlphabetElement.innerText.toLowerCase();
-    // console.log(currentAlphabetElement.innerText);
-    console.log(playerPressed, targetLetter);
+  const playerPressed = event.key;
+  const currentAlphabetElement = document.getElementById("currentAlphabet");
+  const currentAlphabet = currentAlphabetElement.innerText.toLowerCase();
+
+  // check if r8 or wrong
+  if (playerPressed === currentAlphabet) {
+    console.log("You got a point");
+    continueGame();
+    removeClassFromClassListById(currentAlphabet, "bg-orange-400");
+  } else {
+    console.log("fuck you");
+  }
 }
+
 document.addEventListener("keyup", handleKeyUpEvent);
 
 function continueGame() {
-    // generate a random alphabet
-    const randomCharacter = getARandomAlphabet();
-    console.log("your random alphabet is ", randomCharacter);
+  // generate a random alphabet
+  const randomCharacter = getARandomAlphabet();
+  console.log("your random alphabet is ", randomCharacter);
 
-    const currentAlphabetElement = document.getElementById("currentAlphabet");
-    currentAlphabetElement.innerText = randomCharacter;
+  const currentAlphabetElement = document.getElementById("currentAlphabet");
+  currentAlphabetElement.innerText = randomCharacter;
 
-    setBgById(randomCharacter);
+  addClassToClassListById(randomCharacter, "bg-orange-400");
+}
+
+//
+
+function play() {
+  hideElementById("home-screen");
+  showElementById("play-ground");
+  continueGame();
 }
--- js/utility.js
+++ js/utility.js
@@ -1,17 +1,22 @@
 function getARandomAlphabet() {
-    // create an alphabet array
-    const alphabetString = "abcdefghijklmopqrstuvwxyz";
-    const alphabetArray = alphabetString.split("");
-    // console.log(alphabetArray);
+  // create an alphabet array
+  const alphabetString = "abcdefghijklmopqrstuvwxyz";
+  const alphabetArray = alphabetString.split("");
+  // console.log(alphabetArray);
 
-    // get a random index number from 0 to 25
-    const randomIndex = Math.round(Math.random() * 25 - 0);
-    // console.log(randomIndex, alphabetArray[randomIndex]);
-    return alphabetArray[randomIndex];
+  // get a random index number from 0 to 25
+  const randomIndex = Math.abs(Math.round(Math.random() * 25 - 0));
+//   console.log(randomIndex, alphabetArray[randomIndex]);
+  return alphabetArray[randomIndex];
 }
 // getARandomAlphabet();
 
-function setBgById(elementID) {
-    const element = document.getElementById(elementID);
-    element.classList.add("bg-orange-400");
+// add class to classList
+function addClassToClassListById(elementID, className) {
+  const element = document.getElementById(elementID);
+  element.classList.add(className);
+}
+
+function removeClassFromClassListById(elementID, className) {
+  document.getElementById(elementID).classList.remove(className);
 }
