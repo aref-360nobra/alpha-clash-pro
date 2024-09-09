18f259e808d886217541807cfb8c802205cfe99f
aref-360nobra <aref0171personal@gmail.com>
2024-09-09 23:26:59 +0600

    almost done

--- js/alphaClash.js
+++ js/alphaClash.js
@@ -7,17 +7,37 @@ document.getElementById("playNowBtn").addEventListener("click", function () {
   continueGame();
 });
 function handleKeyUpEvent(event) {
-  const playerPressed = event.key;
+  playerPressed = event.key;
   const currentAlphabetElement = document.getElementById("currentAlphabet");
-  const currentAlphabet = currentAlphabetElement.innerText.toLowerCase();
+  const expectedAlphabet = currentAlphabetElement.innerText.toLowerCase();
+  // console.log(expectedAlphabet);
 
-  // check if r8 or wrong
-  if (playerPressed === currentAlphabet) {
-    console.log("You got a point");
+  // check right or wrong
+  if (playerPressed === expectedAlphabet) {
+    console.log("you got a point");
+    // update score:
+    // get the score element
+    const currentScoreElement = document.getElementById("currentScore");
+    const currentScore = parseInt(currentScoreElement.innerText);
+    const newScore = currentScore + 1;
+    currentScoreElement.innerText = newScore;
+    // increase the score by 1
+    // show the updated score
+
+    removeClassFromClassListById(playerPressed, "bg-orange-400");
     continueGame();
-    removeClassFromClassListById(currentAlphabet, "bg-orange-400");
   } else {
-    console.log("fuck you");
+    console.log("dhurr vaia correct key press koro");
+    // get the current life number
+    // reduce the life count
+    // display the updated life count
+
+    const currentLifeElement = document.getElementById("life");
+    const currentLife = parseInt(currentLifeElement.innerText);
+    const updatedLife = currentLife - 1;
+    currentLifeElement.innerText = updatedLife;
+
+    if (updatedLife === 0) gameOver(); // source: LWS
   }
 }
 
@@ -37,7 +57,34 @@ function continueGame() {
 //
 
 function play() {
-  hideElementById("home-screen");
-  showElementById("play-ground");
+  addClassToClassListById("homeScreen", "hidden");
+  addClassToClassListById("scoreBoard", "hidden");
+  removeClassFromClassListById("playGround", "hidden");
+
+  // reset score and life
+  setTextElementValueByID("life", 4);
+  setTextElementValueByID("currentScore", 0);
+
   continueGame();
 }
+
+function gameOver() {
+  console.log("game over");
+  addClassToClassListById("playGround", "hidden");
+  removeClassFromClassListById("scoreBoard", "hidden");
+
+  const lastScoreElement = document.getElementById("currentScore");
+  const lastScore = lastScoreElement.innerText;
+  console.log("your score is: ", lastScore);
+  document.getElementById("score").innerText = lastScore;
+
+  document.getElementById("playAgain").addEventListener("click", function () {
+    play();
+  });
+}
+
+// function playAgain(){
+//   hideElementById("scoreBoard");
+//   showElementById("play-ground");
+//   continueGame();
+// }
--- js/utility.js
+++ js/utility.js
@@ -1,12 +1,12 @@
 function getARandomAlphabet() {
   // create an alphabet array
-  const alphabetString = "abcdefghijklmopqrstuvwxyz";
+  const alphabetString = "abcdefghijklmnopqrstuvwxyz";
   const alphabetArray = alphabetString.split("");
   // console.log(alphabetArray);
 
   // get a random index number from 0 to 25
-  const randomIndex = Math.abs(Math.round(Math.random() * 25 - 0));
-//   console.log(randomIndex, alphabetArray[randomIndex]);
+  const randomIndex = Math.abs(Math.round(Math.random() * 25 - 1));
+  console.log(randomIndex, alphabetArray[randomIndex]);
   return alphabetArray[randomIndex];
 }
 // getARandomAlphabet();
@@ -14,9 +14,18 @@ function getARandomAlphabet() {
 // add class to classList
 function addClassToClassListById(elementID, className) {
   const element = document.getElementById(elementID);
-  element.classList.add(className);
+  if (element) {
+    element.classList.add(className);
+  } else {
+    console.log(`Element with ID '${elementID}' not found.`);
+  }
 }
 
 function removeClassFromClassListById(elementID, className) {
   document.getElementById(elementID).classList.remove(className);
+}
+
+function setTextElementValueByID(elementID, value) {
+  const element = document.getElementById(elementID);
+  element.innerText = value;
 }
